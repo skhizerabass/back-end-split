4e53dec6c13b7a3b37cb92a877d3c824cc3e7ad0
Syed Khizer Abass <syed.khizer@triumphtech.com.au>
2020-04-18 11:53:01 +0500

    initial commit

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use('/api/user', userRouter);
 /////////////////////////////////////////
 /////////////////////////////////////////
 
+
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
